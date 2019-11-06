098cce892c8c2293b0468dd7790a33c418dcba21
glory9 <53071086+glory9@users.noreply.github.com>
2019-11-06 03:52:11 -0600

    Update bea_s2.py

--- bea_s2.py
+++ bea_s2.py
@@ -1,10 +1,10 @@
-#This code prompts the user for a URL, loops through links at that URL, extracts an HTML file, parses the file using the beautiful soup library...
-#...and prints out the value at a preset location in the file
+#This code prompts the user for a URL, loops through links at that URL, extracts an HTML file, parses the file using the beautiful soup
+# library, and prints out the value at a preset location in the file
 
 # To run this, you can install BeautifulSoup
 # https://pypi.python.org/pypi/beautifulsoup4
 
-# Or download the file
+# Or download the library
 # http://www.py4e.com/code3/bs4.zip
 # and unzip it in the same directory as this file
 
@@ -17,17 +17,16 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')
-x=0
-while x < 7:
+
+for i in range(7):
     html = urllib.request.urlopen(url, context=ctx).read()
     soup = BeautifulSoup(html, "html.parser")
-
     tags = soup('a')
-    val = list()
-    lav = list()
+    val, lav = [], []
+    
     for tag in tags:
         lav.append(tag.contents[0])
         val.append(tag.get('href', None))
     url = val[17]
-    x = x + 1
+    
 print(lav[17])
